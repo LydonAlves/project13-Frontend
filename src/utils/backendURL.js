5894f0e291d04e91ddc8f264557ac2429516564c
LydonAlves <lydonalves@gmail.com>
2024-06-25 17:28:49 +0200

    url updated

--- src/utils/backendURL.js
+++ src/utils/backendURL.js
@@ -1,1 +1,2 @@
-export const backendURL = "https://project13-backend.vercel.app/" 
+// export const backendURL = "https://project13-backend.vercel.app/" 
+export const backendURL = "https://project13-backend-k0cqf13am-lydons-projects.vercel.app" 
