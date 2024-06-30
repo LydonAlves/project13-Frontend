c97f949ca5e26c43016da419427b669bf19f06b7
LydonAlves <lydonalves@gmail.com>
2024-06-30 10:23:30 +0200

    backend url updated

--- src/utils/backendURL.js
+++ src/utils/backendURL.js
@@ -1,2 +1,2 @@
-// export const backendURL = "https://project13-backend.vercel.app/api/v1/"
-export const backendURL = "http://localhost:3000/api/v1/" 
+export const backendURL = "https://project13-backend.vercel.app/api/v1/"
+// export const backendURL = "http://localhost:3000/api/v1/" 
